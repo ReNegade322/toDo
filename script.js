7e79942c26d22f219e96189857edc26d399abb1d
ReNegade <evis2k@mail.ru>
2022-03-05 00:50:34 +0300

    Add check-all-button

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const addTaskBtn = document.getElementById('submit');
 const todosWrapper = document.querySelector('.todo--wrapper');
 const inputText = document.getElementById('task__input--name');
+const checkFiller = document.querySelector('.checkbox--filler');
 
-let tasks;
-!localStorage.tasks ? tasks = [] : tasks = JSON.parse(localStorage.getItem('tasks'));
-
+let tasks = [];
+let checkTask = [];
+let close = [];
 
 function NewTask(description){
     this.description = description;
@@ -15,48 +16,116 @@ function NewTask(description){
 const createTemplate = (task, index) => {
     return `
         <div class="new--task ${task.completed ? 'checked': ''}">
-            <div>
-                <input class="toggle" type="checkbox"> ${task.completed ? 'checked' : ''}     
-                <label class="task__label"><span>${task.description}</span></label>
+            <div class="center">
+                <label class="task__label">
+                    <input class="toggle" type="checkbox"> ${task.completed ? 'checked' : ''}     
+                    <span>${task.description}</span>
+                </label>
             </div>
-            <button class="close"></button>
+            <button class="close" value="${index}" ></button>
         </div>
     `
 } 
 
 const fillHtmlList = () => {
     todosWrapper.innerHTML = "";
+    checkTask = [];
+    close = [];
+    checkFiller.style.display = 'none';
+    
     if(tasks.length > 0){
         tasks.forEach((item, index) => {
-            todosWrapper.innerHTML += createTemplate(item, index)
-        })
-    }
-}
+            todosWrapper.innerHTML += createTemplate(item, index);
+        });
 
+        checkTask = document.querySelectorAll('.toggle');
+
+        checkTask.forEach((checkTask) => {
+            checkTask.addEventListener('click', (event) => {
+                const target = event.target;
+                toggler(target);
+            });
+        });
+
+        close = document.querySelectorAll('.close');
+
+        close.forEach((close) => {
+            close.addEventListener('click', (event) => {
+                const target = event.target;
+                deleter(target);
+            }); 
+        });
+
+        checkFiller.style.display = 'block';
+    };  
+};
 fillHtmlList();
 
-const updateLocal = () => {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-}
 
-addTaskBtn.addEventListener('click', () => {
-    tasks.push(new NewTask(inputText.value));
-    updateLocal();
-    fillHtmlList();
+
+// adding task by hitting enter
+
+addTaskBtn.addEventListener('click', (event) => {
+    if (inputText.value != ''){
+        event.preventDefault();
+        tasks.push(new NewTask(inputText.value));
+        fillHtmlList();
+        inputText.value = '';
+    } else {
+        event.preventDefault();
+    }
 });
 
 
+// adding check-all-button
+
+checkFiller.addEventListener('click', () => {
+    let checkedCounter = 0;
+    checkTask.forEach((taskElem) => {
+        if (taskElem.className.includes("checked")){
+            checkedCounter++;
+        };
+    });   
+
+    checkTask.forEach((taskElem) => {
+        if (checkedCounter == 0) {
+            taskElem.classList.add("checked");
+            taskElem.checked = true;  
+        } else if (checkedCounter == checkTask.length) {
+            taskElem.classList.remove("checked");
+            taskElem.checked = false; 
+        } else {
+            if (taskElem.className.includes("checked")){
+            } else {
+                taskElem.classList.add("checked");
+                taskElem.checked = true;     
+            };
+        };
+    });
+});
+
+
+// adding checked checkboxes
+
+function toggler(checkTask){
+    if (checkTask.className.includes("checked")){
+        checkTask.classList.remove("checked");
+    } else {
+        checkTask.classList.add("checked");
+    }
+} 
+
+
+// deliting task
+
+function deleter(close) {
+    tasks.splice (close.value, 1);
+    fillHtmlList();
+}
 
 
 
 
-//получаю текст из инпута
-
-
-//записываю контент в массив
-
-
-// добавление таски в html
 
 
 
@@ -64,3 +133,16 @@ addTaskBtn.addEventListener('click', () => {
 
 
 
+
+
+// checkFiller.addEventListener('click', () => {
+//     checkTask.forEach((taskElem) => {
+//         if (taskElem.className.includes("checked")){
+//             taskElem.classList.remove("checked");
+//             taskElem.checked = false; 
+//         } else {
+//             taskElem.classList.add("checked");
+//             taskElem.checked = true;            
+//         };
+//     });    
+// });
